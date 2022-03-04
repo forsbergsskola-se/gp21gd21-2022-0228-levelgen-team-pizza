8e7450f1c7ac49ab6a497aabed6162669873dfa5
JonathanJonsson <jonathan.jonsson@student.forsbergsskola.se>
2022-03-04 18:27:52 +0100

    Copied a room prefab that is square, set up dungeon script, fixed folder structure for JJ

---
+++ Assets/Programmers/Jonathan/Scripts/DungeonGeneratorJJ.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonGeneratorJJ : MonoBehaviour
+{
+
+    public class Cell
+    {
+        public bool Visited;
+        public bool[] neighbourStatus = new bool[4]; //0 = up, 1=down, 2=Right, 3=left
+
+    }
+
+    void MazeCreator()
+    {
+
+    }
+
+    void CheckNeighbours()
+    {
+
+    }
+
+
+}
