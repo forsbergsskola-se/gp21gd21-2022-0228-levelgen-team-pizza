0d8532feb2c41a7f16c004cc5dd39bbda1c3ab39
JonathanJonsson <jonathan.jonsson@student.forsbergsskola.se>
2022-03-04 18:50:34 +0100

    set up dungeon grid creation

--- Assets/Programmers/Jonathan/Scripts/DungeonGeneratorJJ.cs
+++ Assets/Programmers/Jonathan/Scripts/DungeonGeneratorJJ.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 
 public class DungeonGeneratorJJ : MonoBehaviour
@@ -8,14 +10,50 @@ public class DungeonGeneratorJJ : MonoBehaviour
     public class Cell
     {
         public bool Visited;
-        public bool[] neighbourStatus = new bool[4]; //0 = up, 1=down, 2=Right, 3=left
+        public bool[] neighbourDirection = new bool[4]; //0 = up, 1=down, 2=Right, 3=left
 
+    }
+
+    public Vector2 gridSize;
+    private int startPos = 0;
+
+    private List<Cell> grid;
+
+    private void Start()
+    {
+        MazeCreator();
     }
 
     void MazeCreator()
     {
+        grid = new List<Cell>();
+
+        for (int i = 0; i < gridSize.x; i++)
+        {
+            for (int j = 0; j < gridSize.y; j++)
+            {
+                grid.Add(new Cell());
+            }
+        }
+
+        int currentCell = startPos;
+
+        Stack<int> path = new Stack<int>();
+
+        Debug.Log(grid.Count);
+
+
+
+
+
+
+
+
+
 
     }
+
+
 
     void CheckNeighbours()
     {
