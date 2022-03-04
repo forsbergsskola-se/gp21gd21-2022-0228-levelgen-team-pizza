2e380492d3b6150561507acd4f9139ed3f945256
JonathanJonsson <jonathan.jonsson@student.forsbergsskola.se>
2022-03-04 19:16:04 +0100

    Implemented get neighbour method and verified that corners, edges and middle cells return the correct number of neighbours

--- Assets/Programmers/Jonathan/Scripts/DungeonGeneratorJJ.cs
+++ Assets/Programmers/Jonathan/Scripts/DungeonGeneratorJJ.cs
@@ -32,19 +32,26 @@ public class DungeonGeneratorJJ : MonoBehaviour
         {
             for (int j = 0; j < gridSize.y; j++)
             {
-                grid.Add(new Cell());
+                grid.Add(new Cell()); // add a cell to each spot in the grid
             }
         }
 
-        int currentCell = startPos;
+        int currentCell = startPos; // 0
 
-        Stack<int> path = new Stack<int>();
-
-        Debug.Log(grid.Count);
+        Stack<int> path = new Stack<int>(); // stack for backtracking and keeping a path through the generated maze
 
 
+        int k = 0;
 
+        while (k<1000) // this controls size --> should probably be modified to handle dynamic instantiation
+        {
+            k++;
+            grid[currentCell].Visited = true;
+            List<int> neighbours = CheckNeighbours(currentCell);
 
+            Debug.Log(neighbours.Count);
+
+        }
 
 
 
@@ -55,8 +62,32 @@ public class DungeonGeneratorJJ : MonoBehaviour
 
 
 
-    void CheckNeighbours()
+    List<int> CheckNeighbours(int cell) // LIST<INT> = NEIGHBOURS INT
     {
+        List<int> neighbour = new List<int>();
+
+        //Up neighbour
+        if (cell - gridSize.x >= 0 && !grid[Mathf.FloorToInt(cell - gridSize.x)].Visited)
+        {
+            neighbour.Add(Mathf.FloorToInt(cell - gridSize.x));
+        }
+        //Down neighbour
+        if (cell + gridSize.x < grid.Count && !grid[Mathf.FloorToInt(cell + gridSize.x)].Visited)
+        {
+            neighbour.Add(Mathf.FloorToInt(cell + gridSize.x));
+        }
+
+        //Right Neighbour
+        if ((cell+1) % gridSize.x !=0 && !grid[Mathf.FloorToInt(cell + 1)].Visited) //  cell%gridsize.x = gridsize.x-1 for the rightmost column --> +1 to each side --> cell+1%gridsize.x = gridsize.x = 0 if on rightmost column. See min 20:00 on https://www.youtube.com/watch?v=gHU5RQWbmWE&t=157s
+        {
+            neighbour.Add(Mathf.FloorToInt(cell + 1));
+        }
+        //Left Neighbour
+        if (cell % gridSize.x !=0 && !grid[Mathf.FloorToInt(cell - 1)].Visited)
+        {
+            neighbour.Add(Mathf.FloorToInt(cell -1));
+        }
+        return neighbour;
 
     }
 
