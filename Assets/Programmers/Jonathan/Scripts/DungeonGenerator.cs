951f7341cdd97dc427b61f91941a61265b62f8de
JonathanJonsson <jonathan.jonsson@student.forsbergsskola.se>
2022-03-07 14:35:10 +0100

    Added roomspawner prefab

--- Assets/Programmers/Jonathan/Scripts/DungeonGenerator.cs
+++ Assets/Programmers/Jonathan/Scripts/DungeonGenerator.cs
@@ -20,12 +20,12 @@ public class DungeonGenerator : MonoBehaviour
     public Vector2 offset;
 
 
-    private readonly List<Vector2> OccupiedSpots = new List<Vector2>();
+    private readonly List<Vector2> occupiedSpots = new List<Vector2>();
 
     private void Start()
     {
         var startRoom = Instantiate(this.startRoom, new Vector3(0, 0, 0), Quaternion.identity).GetComponent<SectionHandler>(); // spawn start room at 0,0,0
-        OccupiedSpots.Add(new Vector2(startRoom.roomCoords.x,startRoom.roomCoords.y));
+        occupiedSpots.Add(new Vector2(startRoom.roomCoords.x,startRoom.roomCoords.y));
     }
 
     public void GenerateRoom(RoomDirection roomDir, Vector2 roomCoords)
@@ -33,14 +33,14 @@ public class DungeonGenerator : MonoBehaviour
 
         if (roomDir == RoomDirection.Down)
         {
-            if (!OccupiedSpots.Contains(new Vector2(roomCoords.x, roomCoords.y + 1))) // if there is no room in the next spot to spawn in
+            if (!occupiedSpots.Contains(new Vector2(roomCoords.x, roomCoords.y + 1))) // if there is no room in the next spot to spawn in
             {
                 //spawn room
                 var room = Instantiate(downRooms[Random.Range(0,downRooms.Count)], new Vector3(roomCoords.x*offset.x, 0, -(roomCoords.y + 1)*offset.y), Quaternion.identity).GetComponent<SectionHandler>();
                 //update room cords on room
                 room.roomCoords = new Vector2(roomCoords.x, roomCoords.y + 1);
                 //Add the new spot to the occupied spots list
-                OccupiedSpots.Add(new Vector2(roomCoords.x, roomCoords.y + 1));
+                occupiedSpots.Add(new Vector2(roomCoords.x, roomCoords.y + 1));
             }
             //TODO: REMOVE DEBUG STATEMENTS
             else
@@ -51,11 +51,11 @@ public class DungeonGenerator : MonoBehaviour
 
         if (roomDir == RoomDirection.Up)
         {
-            if (!OccupiedSpots.Contains(new Vector2(roomCoords.x, roomCoords.y - 1)))
+            if (!occupiedSpots.Contains(new Vector2(roomCoords.x, roomCoords.y - 1)))
             {
                 var room = Instantiate(upRooms[Random.Range(0,upRooms.Count)], new Vector3(roomCoords.x*offset.x, 0, -(roomCoords.y - 1)*offset.y), Quaternion.identity).GetComponent<SectionHandler>();
                 room.roomCoords = new Vector2(roomCoords.x, roomCoords.y - 1);
-                OccupiedSpots.Add(new Vector2(roomCoords.x, roomCoords.y - 1));
+                occupiedSpots.Add(new Vector2(roomCoords.x, roomCoords.y - 1));
             }
             else
             {
@@ -65,11 +65,11 @@ public class DungeonGenerator : MonoBehaviour
 
         if (roomDir == RoomDirection.Right)
         {
-            if (!OccupiedSpots.Contains(new Vector2(roomCoords.x + 1, roomCoords.y)))
+            if (!occupiedSpots.Contains(new Vector2(roomCoords.x + 1, roomCoords.y)))
             {
                 var room = Instantiate(rightRooms[Random.Range(0,rightRooms.Count)], new Vector3((roomCoords.x + 1)*offset.x, 0, -roomCoords.y*offset.y), Quaternion.identity).GetComponent<SectionHandler>();
                 room.roomCoords = new Vector2(roomCoords.x + 1, roomCoords.y);
-                OccupiedSpots.Add(new Vector2(roomCoords.x + 1, roomCoords.y));
+                occupiedSpots.Add(new Vector2(roomCoords.x + 1, roomCoords.y));
             }
             else
             {
@@ -79,11 +79,11 @@ public class DungeonGenerator : MonoBehaviour
 
         if (roomDir == RoomDirection.Left)
         {
-            if (!OccupiedSpots.Contains(new Vector2(roomCoords.x - 1, roomCoords.y)))
+            if (!occupiedSpots.Contains(new Vector2(roomCoords.x - 1, roomCoords.y)))
             {
                 var room = Instantiate(leftRooms[Random.Range(0,leftRooms.Count)], new Vector3((roomCoords.x - 1)*offset.x, 0, -roomCoords.y*offset.y), Quaternion.identity).GetComponent<SectionHandler>();
                 room.roomCoords = new Vector2(roomCoords.x - 1, roomCoords.y);
-                OccupiedSpots.Add(new Vector2(roomCoords.x - 1, roomCoords.y));
+                occupiedSpots.Add(new Vector2(roomCoords.x - 1, roomCoords.y));
             }
             else
             {
--- Assets/Programmers/Jonathan/Scripts/SectionHandler.cs
+++ Assets/Programmers/Jonathan/Scripts/SectionHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 /// <summary>
@@ -6,4 +7,11 @@ using UnityEngine;
 public class SectionHandler : MonoBehaviour
 {
     [HideInInspector]public Vector2 roomCoords;
+
+    private void OnDestroy()
+    {
+
+
+
+    }
 }
