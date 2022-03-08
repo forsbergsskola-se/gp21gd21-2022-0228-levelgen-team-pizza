f0e52388ca6aa03cfe649984058433b6c3779765
Armin Afazeli <arminafa84@gmail.com>
2022-03-08 17:43:41 +0100

    Enemy Spawner AA modified

--- Assets/Programmers/Armin/Scripts/EnemySpawnerAA.cs
+++ Assets/Programmers/Armin/Scripts/EnemySpawnerAA.cs
@@ -8,48 +8,48 @@ public class EnemySpawnerAA : MonoBehaviour
     private Transform[] spawnZones;
 
     [SerializeField]
-    private GameObject[] enemies;
-    private int spawnNum;
-    private int enemyNum;
-    private GameObject enemy;
-    private Transform enemySpawn;
+    private GameObject[] enemyPrefabs;
 
-    private void Awake()
-    {
-        enemies[0].SetActive(false);
-        enemies[1].SetActive(false);
-        enemies[2].SetActive(false);
-    }
+    [SerializeField]
+    private List<GameObject> enemyList = new List<GameObject>();
 
-    private void Start()
-    {
-        spawnNum = Random.Range(0, spawnZones.Length);
-        enemyNum = Random.Range(0, enemies.Length);
+    [SerializeField]
+    private int maxSpawn;
 
-        enemy = enemies[enemyNum];
-        enemySpawn = spawnZones[spawnNum];
-
-        enemy.transform.position = enemySpawn.transform.position;
-        enemy.SetActive(true);
-
-    }
+    public bool coroutineRunning;
 
     private void Update()
     {
-        StartCoroutine(EnemyChange());
+        SpawnEnemies();
+        if (!coroutineRunning)
+        {
+            StartCoroutine(EnemyChange());
+        }
     }
 
-    IEnumerator EnemyChange()
+
+    private void SpawnEnemies()
     {
 
-        yield return new WaitForSeconds(3); 
-        enemy.SetActive(false);
-        spawnNum = Random.Range(0, spawnZones.Length);
-        enemyNum = Random.Range(0, enemies.Length);
-        enemy = enemies[enemyNum];
-        enemySpawn = spawnZones[spawnNum];
+        var spawnNum = Random.Range(0, spawnZones.Length - 1);
+        var enemyNum = Random.Range(0, enemyPrefabs.Length - 1);
 
-        enemy.transform.position = enemySpawn.transform.position;
-        enemy.SetActive(true);
+
+        while(enemyList.Count < maxSpawn)
+        {
+            enemyList.Add(Instantiate(enemyPrefabs[enemyNum], spawnZones[spawnNum].transform.position, Quaternion.identity));
+        }
+
+
+    }
+
+    private IEnumerator EnemyChange()
+    {
+
+        yield return new WaitForSeconds(3); // 3 second delay
+        enemyList.RemoveAt(0);
+        Destroy(enemyList[0]);
+        coroutineRunning = false;
+
     }
 }
