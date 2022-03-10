60d5ca45c569591526c2d156881e7898211d9d1a
MaximThellenberg <maxim.thellenberg@student.forsbergsskola.se>
2022-03-10 17:34:06 +0100

    Hp Bar Test Done

--- Assets/Programmers/Pavel/Scripts/PlayerHealth_PS.cs
+++ Assets/Programmers/Pavel/Scripts/PlayerHealth_PS.cs
@@ -7,12 +7,18 @@ public class PlayerHealth_PS : MonoBehaviour{
     [SerializeField] int maxHealth = 100;
     [SerializeField] int currentHealth;
 
+    public PlayerHealthBarMT playerHealthBarMT;
+
+
+
     void Start(){
         currentHealth = maxHealth;
+        playerHealthBarMT.SetMaxHealth(maxHealth);
     }
 
     public void PlayerTakeDamage(int Damage){
         currentHealth -= Damage;
+        playerHealthBarMT.SetHealth(currentHealth);
     }
 
     public void ReloadOnDeath(int Damage)
@@ -22,4 +28,8 @@ public class PlayerHealth_PS : MonoBehaviour{
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
+}
+
+public class HealthBar
+{
 }
