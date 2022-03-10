bbf4f95e5f942a9b5f32f0178784363e1c52a6d7
Armin Afazeli <90157472+Armin-AF@users.noreply.github.com>
2022-03-10 12:27:08 +0100

    Pavel Player Movement Implemented

--- Assets/Programmers/Jonathan/Scripts/SimpleMove.cs
+++ Assets/Programmers/Jonathan/Scripts/SimpleMove.cs
@@ -5,41 +5,22 @@ using UnityEngine;
 
 public class SimpleMove : MonoBehaviour
 {
-    [SerializeField] float speed = 15.0f;
-    [SerializeField] float rotationSpeed = 30.0f;
+    [SerializeField] float speed;
+    [SerializeField] float rotationSpeed;
 
-    [SerializeField] private Rigidbody rb;
+    Rigidbody rb;
 
-
-
-
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     void FixedUpdate()
     {
-        //float mH = Input.GetAxis("Horizontal");
-        //float mV = Input.GetAxis("Vertical");
+        float mh = Input.GetAxis("Horizontal");
+        float mv = Input.GetAxis("Vertical");
 
-        //transform.forward(speed * Time.deltaTime * Input.GetAxis("Vertical"));
-
-        //Quaternion deltaRotation = Quaternion.Euler(Vector3.up * rotationSpeed * Time.fixedDeltaTime * mH);
-       // rb.MoveRotation(rb.rotation * deltaRotation);
-
-       //transform.Rotate(0f, rotationSpeed * Time.deltaTime * mH, 0f);
-       Accelerate();
-       rb.velocity = rb.velocity.normalized * speed;
-       Rotate();
-
-
-    }
-
-    public void Rotate()
-    {
-        var _deltaRotation = Quaternion.Euler( Input.GetAxis("Horizontal")* Time.fixedDeltaTime * new Vector3(0f,rotationSpeed, 0f));
-        rb.MoveRotation(rb.rotation * _deltaRotation);
-    }
-
-    public void Accelerate()
-    {
-        rb.AddForce(Input.GetAxis("Vertical") * speed * transform.forward, ForceMode.VelocityChange);
+        rb.velocity = (transform.forward * mv) * speed * Time.deltaTime;
+        transform.Rotate((transform.up * mh) * rotationSpeed * Time.deltaTime);
     }
 }
