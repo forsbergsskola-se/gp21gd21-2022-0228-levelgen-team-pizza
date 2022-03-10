8a0a59d59e036844ceaf648898226b6dd923ec78
Pavel Sarmiento <pavelsarmiento@hotmail.com>
2022-03-10 12:22:53 +0100

    Add: Better playerMovement

--- Assets/Programmers/Pavel/Scripts/PlayerMovemnt_PS.cs
+++ Assets/Programmers/Pavel/Scripts/PlayerMovemnt_PS.cs
@@ -5,24 +5,22 @@ using UnityEngine;
 
 public class PlayerMovemnt_PS : MonoBehaviour{
 
-    [SerializeField] Rigidbody rb;
-    [SerializeField] int speed;
 
-    Vector3 inputVector;
-    float mh = Input.GetAxisRaw("Horizontal");
-    float mv = Input.GetAxisRaw("Vertical");
+    [SerializeField] float speed;
+    [SerializeField] float rotationSpeed;
 
-    void Start(){
+     Rigidbody rb;
+
+     void Start(){
         rb = GetComponent<Rigidbody>();
     }
 
-    void Update(){
-        inputVector = new Vector3(mh * speed,rb.velocity.y,mv * speed);
-        transform.LookAt(transform.position + new Vector3(inputVector.x,0,inputVector.z));
-    }
-
     void FixedUpdate(){
-        rb.velocity = inputVector;
+        float mh = Input.GetAxis("Horizontal");
+        float mv = Input.GetAxis("Vertical");
+
+        rb.velocity = (transform.forward * mv) * speed * Time.deltaTime;
+        transform.Rotate((transform.up * mh) * rotationSpeed * Time.deltaTime);
     }
 }
 
