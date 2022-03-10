4cf60cd6c85e0cb3211cfd4b06837cb4feb316c2
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-03-10 10:29:22 +0100

    Add IDamagable

--- Assets/Programmers/Armin/Scripts/Health.cs
+++ Assets/Programmers/Armin/Scripts/Health.cs
@@ -2,22 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Health : MonoBehaviour
+public class Health : MonoBehaviour, IDamageble
 {
     [SerializeField] private int currentHealth, maxHealth;
     private bool isEntityDead;
-
 
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
         if (currentHealth <= 0 && isEntityDead == false)
         {
-            gameObject.SetActive(false);
             isEntityDead = true;
+            Destroy(this);
         }
-
-        
     }
-
 }
---
+++ Assets/Programmers/Armin/Scripts/IDamageble.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDamageble
+{
+
+    public abstract void TakeDamage(int damage);
+}
